2539df22d68981e74e532a92146f29f8da2cc9b8
Bilal-12-2 <91077424+Bilal-12-2@users.noreply.github.com>
2022-09-09 17:42:49 +0500

    Progress

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,22 @@
-import {React, useState} from 'react'
-import '../navbar/navbar.css'
-import logo from '../../assets/logo.svg'
-import { Fa500Px } from "react-icons/fa";
+import {React, useState} from 'react';
+import '../navbar/navbar.css';
+import logo from '../../assets/logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
+
+const Menu = ()=>{
+  return(
+  <>
+    <p><a href="#">Home</a></p>
+          <p><a href="#">What is GPT?</a></p>
+          <p><a href="#">Open AI</a></p>
+          <p><a href="#">Case Studies</a></p>
+          <p><a href="#">Library</a></p>
+  </>
+  )
+}
 
 const Navbar = () => {
   const [togglebutton, setToggleButton] = useState(false);
@@ -14,11 +28,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt3-navbar-links-container">
-          <p><a href="#">Home</a></p>
-          <p><a href="#">What is GPT?</a></p>
-          <p><a href="#">Open AI</a></p>
-          <p><a href="#">Case Studies</a></p>
-          <p><a href="#">Library</a></p>
+         <Menu/>
         </div>
       </div>
 
@@ -27,14 +37,26 @@ const Navbar = () => {
         <button type='button'>Sign Up</button>
       </div>
 
-      <div className="gpt3-navbar-menu">
-      {togglebutton
-      ? <Fa500Px  className="icon" onClick={()=>setToggleButton(true)} />
-      : <Fa500Px  className="icon" onClick={()=>setToggleButton(false)} />
-      }
-        
+      <div className="gpt3-navbar-menu" >
+    {togglebutton
+      ? <FontAwesomeIcon icon={faXmark} className="icon" size="2x" onClick={()=>setToggleButton(false)} />
+      : <FontAwesomeIcon icon={faBars} size="2x" className="icon"   onClick={()=>setToggleButton(true)} />
+      } 
+
+      {togglebutton && (
+        <div className="gpt3-navbar-menu-container scale-up">
+         <div className="gpt3-navbar-menu-container-links">
+         <Menu/>
+         <div className="gpt3-navbar-menu-container-links-sign">
+        <p>Sign in</p>
+        <button type='button'>Sign Up</button>
+      </div>
+          </div> 
+        </div>
+      )}
       </div>
     </div>
+   
 
     </>
    
