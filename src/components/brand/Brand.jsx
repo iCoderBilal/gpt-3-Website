59814829d38b820908dcbb60c273dc3254ee5069
Bilal-12-2 <91077424+Bilal-12-2@users.noreply.github.com>
2022-09-15 04:26:03 +0500

    hello

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import '../brand/brand.css'
+import React from 'react';
+import '../brand/brand.css';
+import {google, slack, atlassian, dropbox, shopify} from './import';
+
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+ 
+    <div className="gpt3-brand section__padding">
+      <div>
+        <img src={google} alt=" google" />
+      </div>
+
+      <div>
+        <img src={slack} alt=" google" />
+      </div>
+
+      <div>
+        <img src={atlassian} alt=" google" />
+      </div>
+
+      <div>
+        <img src={dropbox} alt=" google" />
+      </div>
+
+      <div>
+        <img src={shopify} alt=" google" />
+      </div>
+    </div>
+   
   )
 }
 
